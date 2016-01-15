3c3cb8e2b10ba4ce75f3c2fed9699c4e0f81bc81
unknown <alberto.vaccari108@gmail.com>
2016-01-15 16:13:20 +0200

    added end of the world collider

---
+++ Assets/Scripts/EndOfWorld.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EndOfWorld : MonoBehaviour
+{
+	
+	public int damage = int.MaxValue;		// Insta-death if player falls outside the level boundaries
+	private Player _player;
+
+	void Start ()
+	{
+		_player = GameObject.FindGameObjectWithTag ("Player").GetComponent<Player> ();
+		
+		if (_player == null)
+			Debug.LogWarning ("Spikes: Player not found. Did you forget setting the tag, mate?");
+
+	}
+
+	void Update ()
+	{
+
+	}
+	
+	void OnTriggerEnter2D (Collider2D col)
+	{
+
+		// If the player is the colliding object, kill player
+		if (col.CompareTag ("Player"))
+			_player.DealDamage (damage);
+
+	}
+}
